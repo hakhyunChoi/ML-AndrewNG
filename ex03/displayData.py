d850277bf65863b4e214584e98afd436fe95736a
quant <quant@quant-desktop>
2017-05-02 18:11:29 +0900

    20170502_1004

---
+++ ex03/displayData.py
@@ -0,0 +1,60 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def displayData(X, example_width=None):
+    #DISPLAYDATA Display 2D data in a nice grid
+    #   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
+    #   stored in X in a nice grid. It returns the figure handle h and the 
+    #   displayed array if requested.
+    
+    # Set example_width automatically if not passed in
+    if not example_width:
+        example_width = int( round(np.sqrt(X.shape[1])) )
+    
+    plt.close()
+    plt.figure()
+    # Gray Image
+    plt.set_cmap('gray')
+    
+    # Compute rows, cols
+    m, n = X.shape
+    example_height = int(n / example_width)
+    
+    # Compute number of items to display
+    display_rows = int( math.floor(math.sqrt(m)) )
+    display_cols = int( math.ceil(m / display_rows) )
+    
+    # Between images padding
+    pad = 1
+    
+    # Setup blank display
+    display_array = - np.ones( (pad + display_rows * (example_height + pad), pad + display_cols * (example_width + pad)) )
+
+    # Copy each example into a patch on the display array
+    curr_ex = 1
+    for j in range(1, display_rows+1):
+        for i in range(1, display_cols+1):
+            if curr_ex > m:
+                break 
+            # Copy the patch
+            
+            # Get the max value of the patch
+            max_val = max(abs(X[curr_ex-1, :]))
+            rows    = pad + (j - 1) * (example_height + pad) + np.array(range(example_height))
+            cols    = pad + (i - 1) * (example_width  + pad) + np.array(range(example_width ))
+            
+#             display_array[rows, cols] = np.reshape( X[curr_ex, :], example_height, example_width ) / max_val
+            display_array[rows[0]:rows[-1]+1 , cols[0]:cols[-1]+1] = np.reshape(X[curr_ex-1, :], (example_height, example_width), order="F") / max_val
+            curr_ex += 1
+        
+        if curr_ex > m:
+            break
+    # Display Image
+    h = plt.imshow(display_array, vmin=-1, vmax=1 )
+    
+    # Do not show axis
+    plt.axis('off')
+    plt.show(block=False)
+    
+    return h, display_array
--- ex03/ex3.py
+++ ex03/ex3.py
@@ -1,1 +1,70 @@
 import numpy as np
+import scipy.io
+
+
+## User defined functions
+import displayData as dd
+import oneVsAll as ova
+
+## Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
+
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the
+#  linear exercise. You will need to complete the following functions 
+#  in this exericse:
+#
+#     lrCostFunction.m (logistic regression cost function)
+#     oneVsAll.m
+#     predictOneVsAll.m
+#     predict.m
+#
+#  For this exercise, you will not need to change any code in this file,
+#  or any other files other than those mentioned above.
+#
+
+## Setup the parameters you will use for this part of the exercise
+input_layer_size    = 400   # 20x20 Input Images of Digits
+num_labels          = 10    # 10 labels, from 1 to 10   
+# (note that we have mapped "0" to label 10)
+
+## =========== Part 1: Loading and Visualizing Data =============
+#  We start the exercise by first loading and visualizing the dataset. 
+#  You will be working with a dataset that contains handwritten digits.
+
+# Load Training Data
+print('Loading and Visualizing Data ...\n')
+
+mat             = scipy.io.loadmat('ex3data1.mat') # training data stored in arrays X, y
+X, y            = mat['X'], mat['y']
+m               = X.shape[0]
+
+# Randomly select 100 data points to display
+rand_indices    = np.random.permutation(m)
+sel             = X[rand_indices[0:100], :]
+
+dd.displayData(sel)
+
+input('Program paused. Press enter to continue.')
+
+## ============ Part 2: Vectorize Logistic Regression ============
+#  In this part of the exercise, you will reuse your logistic regression
+#  code from the last exercise. You task here is to make sure that your
+#  regularized logistic regression implementation is vectorized. After
+#  that, you will implement one-vs-all classification for the handwritten
+#  digit dataset.
+#
+ 
+print('\nTraining One-vs-All Logistic Regression...')
+ 
+lambda_reg = 0.1
+all_theta = ova.oneVsAll(X, y, num_labels, lambda_reg)
+input('Program paused. Press enter to continue.')
+
+
+# ## ================ Part 3: Predict for One-Vs-All ================
+# #  After ...
+# pred = predictOneVsAll(all_theta, X);
+# 
+# fprintf('\nTraining Set Accuracy: #f\n', mean(double(pred == y)) * 100);
